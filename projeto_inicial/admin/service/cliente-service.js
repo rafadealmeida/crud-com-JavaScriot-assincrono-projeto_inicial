46af6eee3696e8191702652c414729e2a119f625
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-03-28 15:07:53 -0300

    Update cliente-service.js

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,30 +1,40 @@
-const http = new XMLHttpRequest();
 
-const conteudo = (nome,email) => {
-        
+const criarNovaLinha = (nome,email) => {
+    
     const novaLinhaCliente = document.createElement('tr');
     const conteudo =
     
-        `
-        <td class="td" data-td>${nome}</td>
-        <td>${email}</td>
-        <td>
-        <ul class="tabela__botoes-controle">
-        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-        </td> 
-        `
-
-        novaLinhaCliente.innerHTML=conteudo;
-        return novaLinhaCliente;
+    `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+    <ul class="tabela__botoes-controle">
+    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+    <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+    </ul>
+    </td> 
+    `
+    
+    novaLinhaCliente.innerHTML=conteudo;
+    return novaLinhaCliente;
 }
+
+
+const tabela = document.querySelector('[data-tabela]');
+
+
+const http = new XMLHttpRequest();
 
 http.open('GET','http://localhost:3000/profile');
 
 http.send();
 
 http.onload = () =>{
-    const data = http.response;
+    const data = JSON.parse(http.response);
     console.log(data);
+    data.forEach(elemento =>{
+        tabela.appendChild(criarNovaLinha(elemento.nome, elemento.email));
+        
+    })
+    
 }
