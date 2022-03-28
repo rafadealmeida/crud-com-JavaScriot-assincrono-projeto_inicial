bf881d9d0a7721b3f0980d3c4338d8fdfe8750e6
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-03-28 14:44:37 -0300

    Criando elemento html via JS

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,5 +1,25 @@
 const http = new XMLHttpRequest();
 
+const conteudo = (nome,email) => {
+        
+    const novaLinhaCliente = document.createElement('tr');
+    const conteudo =
+    
+        `
+        <td class="td" data-td>${nome}</td>
+        <td>${email}</td>
+        <td>
+        <ul class="tabela__botoes-controle">
+        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        </ul>
+        </td> 
+        `
+
+        novaLinhaCliente.innerHTML=conteudo;
+        return novaLinhaCliente;
+}
+
 http.open('GET','http://localhost:3000/profile');
 
 http.send();
