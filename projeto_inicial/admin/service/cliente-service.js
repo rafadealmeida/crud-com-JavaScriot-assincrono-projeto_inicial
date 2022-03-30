c7f56faa26541a60db920e09a76ed932ae5d62f3
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-03-30 16:13:39 -0300

    Utilizando fetch e aplicando modulos no JS

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,52 +1,35 @@
 
-const criarNovaLinha = (nome,email) => {
-    
-    const novaLinhaCliente = document.createElement('tr');
-    const conteudo =
-    
-    `
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-    <ul class="tabela__botoes-controle">
-    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-    <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-    </ul>
-    </td> 
-    `
-    
-    novaLinhaCliente.innerHTML=conteudo;
-    return novaLinhaCliente;
-}
-
-
-const tabela = document.querySelector('[data-tabela]');
-
 
 const listaCliente= () =>{
-    const promise = new Promise((resolve, reject) =>{
-        const http = new XMLHttpRequest();
-
-        http.open('GET','http://localhost:3000/profile');
-
-        http.onload = () =>{
-        
-           if(http.status>= 400){
-               reject(JSON.parse(http.response));
-           }
-           else{
-               resolve(JSON.parse(http.response)); 
-           }
-                
-        }
-        http.send();
+   
+    return fetch(`http//localhost:3000/profile`)
+ 
+    .then(resposta => {
+        return resposta.json()
     })
-    return promise;   
-}
+    
+     // const promise = new Promise((resolve, reject) =>{
+     //     const http = new XMLHttpRequest();
+ 
+     //     http.open('GET','http://localhost:3000/profile');
+ 
+     //     http.onload = () =>{
+         
+     //        if(http.status>= 400){
+     //            reject(JSON.parse(http.response));
+     //        }
+     //        else{
+     //            resolve(JSON.parse(http.response)); 
+     //        }
+                 
+     //     }
+     //     http.send();
+     // })
+     // return promise;   
+ }
 
-listaCliente()
-.then(data =>{
-     data.forEach(elemento =>{
-        tabela.appendChild(criarNovaLinha(elemento.nome, elemento.email));
-        })
-});
+
+
+ export const clienteService = {
+     listaCliente
+ } 
---
+++ projeto_inicial/controller/listaClientes--controller.js
@@ -0,0 +1,35 @@
+import { clienteService } from "../admin/service/cliente-service.js";
+
+
+const criarNovaLinha = (nome,email) => {
+    
+    const novaLinhaCliente = document.createElement('tr');
+    const conteudo =
+    
+    `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+    <ul class="tabela__botoes-controle">
+    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+    <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+    </ul>
+    </td> 
+    `
+    
+    novaLinhaCliente.innerHTML=conteudo;
+    return novaLinhaCliente;
+}
+
+
+const tabela = document.querySelector('[data-tabela]');
+
+
+
+
+clienteService.listaCliente()
+.then(data =>{
+     data.forEach(elemento =>{
+        tabela.appendChild(criarNovaLinha(elemento.nome, elemento.email));
+        })
+});
